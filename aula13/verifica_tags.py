3dcd7f0834d238dd1fe003e18beecb6cd65d97c8
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-11 09:36:45 -0300

    oi

---
+++ aula13/verifica_tags.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python3 #
+# -*- coding: utf-8 -*- #
+
+
+## Impotação de módulos - - - - - - - - - - - - - - - - - - - - - - - #
+from typing import TextIO # TextIO , biblioteca  para armazenar o documento.
+from collections import deque
+
+
+## Implementação de funções - - - - - - - - - - - - - - - - - - - - - #
+def verifica_tags(documento: TextIO) -> bool:
+    """
+Recebe um documento html e retorna True se, para cada tag, existe uma
+fecha tag correspondente. Caso contrário, retorna False.
+    """
+
+    P = deque() # Criando a pilha vazia
+
+    # percorre linha a linha do documento iterável. O iterador passa linha por linha do html
+    for linha in documento:
+        ## procura por uma tag
+        ## o find retorna o índice de onde foi encontrado o caractere
+        ## '<' na string, caso o caractere não é encotrado, retorna -1
+        j = linha.find('<')
+
+        while j != -1: # se for -1 é porque não encontrou 
+            ## o segundo argumento indica o início da busca
+            k = linha.find('>', j+1)
+            
+            if k == -1:
+                ## -1 indica que a tag não foi escrita de forma correta
+                return False
+
+            ## obtém tag sem os símbolos < e >
+            tag = linha[j+1:k] # Pega o texto que está dentro da tag.
+
+            if not tag.startswith('/'):
+                ## se a tag não começa com o caractere '/', então ela
+                ## é uma tag de aberta e deve ser inserida na pilha
+                P.append(tag)
+
+            else:
+                ## note que se o else é executado, então a tag é uma
+                ## tag de fechamento
+                if len(P) == 0:
+                    ## se a pilha estão vazia, então uma está faltando
+                    ## alguma tag de abertura
+                    return False
+
+                ## aqui é feito tag[1:] pois a string da variável tag
+                ## começa com o caractere '/'
+                if tag[1:] != P.pop():
+                    ## se as tags são distintas, então foi uma parte do
+                    ## documento html foi aberto com p e fechado com
+                    ## div, por exemplo
+                    return False
+
+            ## procura se há outra tag na linha
+            ## se tiver, começa novamente o processo de verificação
+            ## do contrário, vai para a próxima linha
+            j = linha.find('<', k+1)
+
+    ## se a pilha estiver vazia, então todas as tags abertas foram
+    ## fechadas por suas tags correspondentes, caso contrário, há
+    ## tags que não foram fechadas
+    return len(P) == 0 # Consome todos os elementos da pilha, significa que a pilha ta vazia
+
+
+## Script principal - - - - - - - - - - - - - - - - - - - - - - - - - #
+if __name__ == "__main__":
+ 
+    with open("teste.html") as documento:
+        print(verifica_tags(documento))
